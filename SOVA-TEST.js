662bb3b2b937732a61b2ee41b25618c18a1737df
Lukas-dotcom <lukas.tejral@gmail.com>
2025-03-14 15:55:50 +0100

    a

--- SOVA-TEST.js
+++ SOVA-TEST.js
@@ -137,6 +137,8 @@
                     align-items: center;
                     padding: 16px;
                     box-shadow: 0 2px 5px rgba(0,0,0,.2);
+                    font-family: sans-serif;
+
                 `);
         
                 alertDiv.innerHTML = `
