425feb7a62467382e31fefcd1a92e6b14741d801
Lukas-dotcom <lukas.tejral@gmail.com>
2025-03-17 17:20:39 +0100

    Update SOVA-TEST.js

--- SOVA-TEST.js
+++ SOVA-TEST.js
@@ -664,13 +664,14 @@ async function upnutiVerzi() {
 
         // Aktualizace verzí
         let updatedLukeContent = lukeContent.replace(
-            /(src|href)=("|')([^"']+\?v=)(\d+\.\d+)([^"'#]*)(#DEBUG_TIMESTAMP#)?("|')/g, 
+            /(src|href)=("|')([^"'>]+?\?v=)(\d+\.\d+)([^"'>]*)(#DEBUG_TIMESTAMP#)?("|')/g, 
             function(match, attr, quoteStart, url, version, suffix, debug, quoteEnd) {
                 let newVersion = increaseVersion(version);
                 console.log(`[SOVA] Aktualizace: ${version} → ${newVersion}`);
                 return `${attr}=${quoteStart}${url}${newVersion}${suffix}${debug || ""}${quoteEnd}`;
             }
           );
+          
           
 
         let newContent = content.substring(0, startIndex) + updatedLukeContent + content.substring(stopIndex);
