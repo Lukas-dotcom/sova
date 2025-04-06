5f1621cccff9cf928b1ebdcd9cf00a990af52c1c
Lukas-dotcom <152334908+Lukas-dotcom@users.noreply.github.com>
2025-04-06 20:54:46 +0200

    🔄 Aktualizace FE-settings.js

--- FE-settings.js
+++ FE-settings.js
@@ -1,1 +1,81 @@
-window.testovaciPromenna = "ahoj";
+window.shippingHideRules = [
+  {
+    "shippingIds": [
+      29,
+      104
+    ],
+    "exactCodes": [
+      "KOL0001",
+      "KOL0002"
+    ],
+    "startsWithCodes": [
+      "PRT"
+    ],
+    "startsWithUrl": [
+      "/monitory/",
+      "/tiskarny/"
+    ],
+    "containsName": [
+      "AIO",
+      "IMAC",
+      "AiO",
+      "iMAC"
+    ]
+  },
+  {
+    "shippingIds": [
+      37,
+      112
+    ],
+    "exactCodes": [
+      "KOL0001",
+      "KOL0002"
+    ],
+    "startsWithCodes": null,
+    "startsWithUrl": null,
+    "containsName": null
+  },
+  {
+    "shippingIds": [
+      65,
+      140
+    ],
+    "exactCodes": [
+      "KOL0001",
+      "KOL0002"
+    ],
+    "startsWithCodes": [
+      "PRT"
+    ],
+    "startsWithUrl": [
+      "/monitory/",
+      "/tiskarny/"
+    ],
+    "containsName": [
+      "AIO",
+      "IMAC",
+      "AiO",
+      "iMAC"
+    ]
+  },
+  {
+    "shippingIds": [
+      48,
+      123
+    ],
+    "exactCodes": [
+      "KOL0001",
+      "KOL0002"
+    ],
+    "startsWithCodes": [
+      "PRT"
+    ],
+    "startsWithUrl": [
+      "/pocitace/",
+      "/monitory/",
+      "/tiskarny/"
+    ],
+    "containsName": null
+  }
+];
+
