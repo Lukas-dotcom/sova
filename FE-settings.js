6aba6505c561cefdea804e4749ff5e3b86c979b8
Lukas-dotcom <152334908+Lukas-dotcom@users.noreply.github.com>
2025-04-07 04:33:01 +0200

    🔄 Aktualizace FE-settings.js

--- FE-settings.js
+++ FE-settings.js
@@ -10,7 +10,7 @@ window.expedice = [
   {
     "zobrazit": true,
     "test": false,
-    "cas_expedice": 0.5833333333333334,
+    "cas_expedice": "14:00",
     "statni_svatky": [
       "1.1.",
       "1.5.",
