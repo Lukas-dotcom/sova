4d9767dabddefa7d6908d164fe74a0c68da5f9f2
Lukas-dotcom <lukas.tejral@gmail.com>
2025-04-16 00:17:27 +0200

    Update SOVA-TEST.js

--- SOVA-TEST.js
+++ SOVA-TEST.js
@@ -498,7 +498,7 @@ async function sovaParamSortingWorker(currentItem) {
     // Získáme jméno parametru z nadpisu stránky
     await sleep(300);
     let paramNameElem = document.querySelector(".content-header h1 strong");
-    log(`🔍 Název parametru vyčtený ze stránky raw: "${paramNameElem}"`);
+    log(`🔍 Název parametru vyčtený ze stránky raw: "${paramNameElem}"`); 
     if (!paramNameElem) {
         log("⚠️ Nenalezen nadpis parametru.");
         return { shouldSave: false };
