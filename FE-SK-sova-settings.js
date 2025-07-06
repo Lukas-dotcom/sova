2307101f9654eafe93335b51540ba26f561afe73
Lukas-dotcom <152334908+Lukas-dotcom@users.noreply.github.com>
2025-07-06 10:52:59 +0200

    🔄 Aktualizace FE-SK-sova-settings.js

--- FE-SK-sova-settings.js
+++ FE-SK-sova-settings.js
@@ -1,1 +1,198 @@
+window.LadzoSklad = {
+  "LIC0004": true,
+  "LIC0005": true,
+  "LIC0010": true
+};
+
+window.expedice = {
+  "zobrazit": true,
+  "test": false,
+  "cas_expedice": "14:00",
+  "statni_svatky": [
+    "1.1.",
+    "1.5.",
+    "8.5.",
+    "5.7.",
+    "6.7.",
+    "28.9.",
+    "28.10.",
+    "17.11.",
+    "24.12.",
+    "25.12.",
+    "26.12.",
+    "31.12."
+  ],
+  "velikonocni_pondeli": [
+    "21.04.2025",
+    "06.04.2026",
+    "29.03.2027",
+    "17.04.2028",
+    "02.04.2029",
+    "22.04.2030",
+    "14.04.2031",
+    "29.03.2032",
+    "18.04.2033",
+    "10.04.2034"
+  ]
+};
+
+window.shippingHideRules = [
+  {
+    "shippingIds": [
+      66,
+      75
+    ],
+    "exactCodes": [
+      "KOL0001",
+      "KOL0002"
+    ],
+    "startsWithCodes": [
+      "PRT",
+      "AIO"
+    ],
+    "startsWithUrl": [
+      "/monitory/",
+      "/tlaciarne/",
+      "/monitory-2/"
+    ],
+    "containsName": [
+      "AIO",
+      "IMAC",
+      "AiO",
+      "iMAC",
+      "iMac"
+    ]
+  },
+  {
+    "shippingIds": [
+      145,
+      151
+    ],
+    "exactCodes": [
+      "KOL0001",
+      "KOL0002"
+    ],
+    "startsWithCodes": [
+      "PRT",
+      "AIO"
+    ],
+    "startsWithUrl": [
+      "/monitory/",
+      "/tlaciarne/",
+      "/monitory-2/"
+    ],
+    "containsName": [
+      "AIO",
+      "IMAC",
+      "AiO",
+      "iMAC",
+      "iMac"
+    ]
+  },
+  {
+    "shippingIds": [
+      148,
+      154
+    ],
+    "exactCodes": [
+      "KOL0001",
+      "KOL0002"
+    ],
+    "startsWithCodes": [
+      "PRT",
+      "AIO"
+    ],
+    "startsWithUrl": [
+      "/monitory/",
+      "/tlaciarne/",
+      "/monitory-2/"
+    ],
+    "containsName": [
+      "AIO",
+      "IMAC",
+      "AiO",
+      "iMAC",
+      "iMac"
+    ]
+  },
+  {
+    "shippingIds": [
+      127,
+      133
+    ],
+    "exactCodes": [
+      "KOL0001",
+      "KOL0002"
+    ],
+    "startsWithCodes": [
+      "PRT",
+      "AIO"
+    ],
+    "startsWithUrl": [
+      "/monitory/",
+      "/tlaciarne/",
+      "/monitory-2/"
+    ],
+    "containsName": [
+      "AIO",
+      "IMAC",
+      "AiO",
+      "iMAC",
+      "iMac"
+    ]
+  },
+  {
+    "shippingIds": [
+      139,
+      136
+    ],
+    "exactCodes": [
+      "KOL0001",
+      "KOL0002"
+    ],
+    "startsWithCodes": [
+      "PRT",
+      "AIO"
+    ],
+    "startsWithUrl": [
+      "/monitory/",
+      "/tlaciarne/",
+      "/monitory-2/"
+    ],
+    "containsName": [
+      "AIO",
+      "IMAC",
+      "AiO",
+      "iMAC",
+      "iMac"
+    ]
+  }
+];
+
+window.odpocetHome = {
+  "zobrazit": true,
+  "test": false
+};
+
+window.akcniProduktyLevySloupec = {
+  "zobrazit": false,
+  "test": false
+};
+
+window.navstivene = {
+  "zobrazit": false,
+  "test": false
+};
+
+window.dvojbalikyNechat = {
+  "dvojbaliky": true,
+  "maloobchod": [
+    121,
+    148
+  ],
+  "velkoobchod": [
+    142,
+    154
+  ]
+};
 
