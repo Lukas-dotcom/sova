a464ddf50dd25504d3145f050947cb417b652ec3
Lukas-dotcom <lukas.tejral@gmail.com>
2025-07-06 13:53:57 +0200

    Update sova.js

--- sova.js
+++ sova.js
@@ -687,9 +687,9 @@ async function paramSortingSingle() {
 
 
 async function adminDeliveryHelper() {
-    log('🔔 adminDeliveryHelper START');
   /* ---------- utils ---------- */
   const log = (...a) => console.log('%c[SOVA-COD]', 'color:#00aba7;font-weight:bold', ...a);
+  log('🔔 adminDeliveryHelper START');
   const num = t => (t ? parseFloat(t.replace(/[^0-9,\.]/g, '').replace(',', '.')) || 0 : 0);
   const fmt = n => n.toFixed(2).replace('.', ',');
   const norm = s => s.replace(/\s+/g, ' ').trim().toLowerCase();
