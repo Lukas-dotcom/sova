dbd7689388f205408375fe8e2951d67d3d0b109e
Lukas-dotcom <152334908+Lukas-dotcom@users.noreply.github.com>
2025-08-09 19:48:06 +0200

    🔄 Aktualizace FE-sova-settings.js

--- FE-sova-settings.js
+++ FE-sova-settings.js
@@ -16,8 +16,7 @@ window.injectFunctions = [
   {
     "function": "testovaciInjectFunction",
     "parameters": "(\"ano je to \" & productName)",
-    "url": "/unisex-hodinky-nature/",
-    "pageType": "productDetail"
+    "url": "/unisex-hodinky-nature/"
   },
   {
     "function": "testovaciInjectFunction",
