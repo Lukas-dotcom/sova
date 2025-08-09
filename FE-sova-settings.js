871b36363dc3ef317c3b5efd6622316cdd4b9829
Lukas-dotcom <152334908+Lukas-dotcom@users.noreply.github.com>
2025-08-09 19:21:07 +0200

    🔄 Aktualizace FE-sova-settings.js

--- FE-sova-settings.js
+++ FE-sova-settings.js
@@ -21,9 +21,6 @@ window.injectFunctions = [
   {
     "function": "testovaciInjectFunction",
     "conditionsSOVAL": "contains(productCurrentCategory, \"Do domácnosti\")"
-  },
-  {
-    "parameters": "{ \"text\": '\"produkt \" & productName & \"je dražší než 250\"' }"
   }
 ];
 
