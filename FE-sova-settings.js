256d0ea36b9ed7a790663bb0992a04c03f41c4e8
Lukas-dotcom <152334908+Lukas-dotcom@users.noreply.github.com>
2025-08-12 00:45:17 +0200

    🔄 Aktualizace FE-sova-settings.js

--- FE-sova-settings.js
+++ FE-sova-settings.js
@@ -1297,7 +1297,7 @@ window.additionalSale = [
   {
     "code": "NET0034",
     "name": "Přidat wifi",
-    "price": "€20,41",
+    "price": "€5,28",
     "pairText": "wifi",
     "type": "checkbox",
     "SOVAL": "currency=\"EUR\" AND category = \"Počítače\" AND parametroptdisableupgradewifi != \"true\" AND parametrWifi !=\"ANO\""
