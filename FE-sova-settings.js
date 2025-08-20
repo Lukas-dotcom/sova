e3b79384447f0184958b2125a9e815df16dc5528
Lukas-dotcom <152334908+Lukas-dotcom@users.noreply.github.com>
2025-08-20 13:32:31 +0200

    🔄 Aktualizace FE-sova-settings.js

--- FE-sova-settings.js
+++ FE-sova-settings.js
@@ -55,7 +55,6 @@ window.injectVariables = {
     "Dell Latitude 7420",
     "Dell Latitude 7430",
     "Dell Latitude 7320 Detachable",
-    "MSI Modern",
     "MSI Prestige 13 AI Evo A1MG",
     "MSI Prestige 13Evo A13M",
     "MSI Prestige 14Evo B13M",
