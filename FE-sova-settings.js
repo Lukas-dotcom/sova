987c29eb82ee0fb7c73e7ee3679c4003f46d2225
Lukas-dotcom <152334908+Lukas-dotcom@users.noreply.github.com>
2026-05-22 10:26:52 +0200

    🔄 Aktualizace FE-sova-settings.js

--- FE-sova-settings.js
+++ FE-sova-settings.js
@@ -46,7 +46,7 @@ window.injectVariables = {
     "Lenovo ThinkPad X13 Gen 1",
     "Lenovo ThinkPad X380 Yoga",
     "Lenovo ThinkPad X390 Yoga",
-    "Lenovo ThinkPad X1 Carbon 7",
+    "Lenovo ThinkPad X1 Carbon",
     "Lenovo ThinkPad X1 Yoga",
     "Lenovo ThinkPad X1 Extreme",
     "Lenovo ThinkPad P14s"
