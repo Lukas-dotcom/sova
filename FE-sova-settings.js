4b1759f06b1341aaa05945947b251c3eadaf8b98
Lukas-dotcom <152334908+Lukas-dotcom@users.noreply.github.com>
2026-07-08 07:37:52 +0200

    🔄 Aktualizace FE-sova-settings.js

--- FE-sova-settings.js
+++ FE-sova-settings.js
@@ -85,7 +85,7 @@ window.additionalSale = [
     "price": "129 Kč",
     "pairText": "wifi",
     "type": "checkbox",
-    "SOVAL": "(currency=\"CZK\" AND category = \"Počítače\" AND parametrWifi !=\"ANO\")"
+    "SOVAL": "(currency=\"CZK\" AND category = \"Počítače\" AND parametrWifi !=\"ANO\") "
   },
   {
     "code": "NET0036",
@@ -93,7 +93,7 @@ window.additionalSale = [
     "price": "149 Kč",
     "pairText": "bluetooth",
     "type": "checkbox",
-    "SOVAL": "(currency=\"CZK\" AND category = \"Počítače\" AND parametrBluetooth !=\"ANO\")"
+    "SOVAL": "(currency=\"CZK\" AND category = \"Počítače\" AND parametrBluetooth !=\"ANO\") "
   },
   {
     "code": "BLESK1",
