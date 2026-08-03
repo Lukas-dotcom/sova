e39befcbd54b24fa6b4e93c2205aa5748687f56a
Lukas-dotcom <152334908+Lukas-dotcom@users.noreply.github.com>
2026-08-03 12:24:56 +0200

    🔄 Aktualizace FE-sova-settings.js

--- FE-sova-settings.js
+++ FE-sova-settings.js
@@ -1305,7 +1305,7 @@ window.additionalSale = [
   },
   {
     "code": "MAR0021",
-    "name": "Windows 11 PRO ",
+    "name": "Windows 11 PRO",
     "price": "€36,78",
     "pairText": "Lepší Windows",
     "type": "select",
