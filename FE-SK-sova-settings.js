daeb2f1740df083a6c30ec2defe05aa503e1720f
Lukas-dotcom <152334908+Lukas-dotcom@users.noreply.github.com>
2026-08-07 21:25:02 +0200

    🔄 Aktualizace FE-SK-sova-settings.js

--- FE-SK-sova-settings.js
+++ FE-SK-sova-settings.js
@@ -1179,7 +1179,7 @@ window.additionalSale = [
     "price": "€122,00",
     "pairText": "Rozšírenie RAM",
     "type": "select",
-    "SOVAL": "((NOT (contains (productName, \"Chromebook\") OR contains (parametrOperacnysystem, \"Chrome\") OR contains (productName, \"Apple\") OR contains (productName, \"Macbook\") OR contains (productName, \"iMac\") OR parametrVyrobce = \"Apple\" OR contains(productCode,\"AIO\"))) AND (ALL(RAMvylouceneCastiNazvu, NOT CONTAINS(productName, RAMvylouceneCastiNazvu))) AND (category = \"Počítače\" AND ((parametroptdisableupgraderam = \"false\" OR parametrsovaupgradeVRP0034 = \"ANO\")) AND NOT ( CONTAINS(productName, \"All in One\") ) AND NOT ( CONTAINS(parametrTypdisku, \"eMMC\") ) AND CONTAINS(parametrTypoperacnejpamate, \"DDR5\")))"
+    "SOVAL": "((NOT (contains (productName, \"Chromebook\") OR contains (parametrOperacnysystem, \"Chrome\") OR contains (productName, \"Apple\") OR contains (productName, \"Macbook\") OR contains (productName, \"iMac\") OR parametrVyrobce = \"Apple\" OR contains(productCode,\"AIO\"))) AND (ALL(RAMvylouceneCastiNazvu, NOT CONTAINS(productName, RAMvylouceneCastiNazvu))) AND (category = \"Počítače\" AND ((parametroptdisableupgraderam = \"false\" OR parametrsovaupgradeVRP0035 = \"ANO\")) AND NOT ( CONTAINS(productName, \"All in One\") ) AND NOT ( CONTAINS(parametrTypdisku, \"eMMC\") ) AND CONTAINS(parametrTypoperacnejpamate, \"DDR5\")))"
   },
   {
     "code": "VRP0036",
